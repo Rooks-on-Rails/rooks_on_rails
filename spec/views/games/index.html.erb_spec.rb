c048207dd7d52f3ad89d6f1a99ede7b49697275b
dpoit <dannypoit@gmail.com>
2017-10-04 14:32:07 -0400

    Fixed rubocop offenses

--- spec/views/games/index.html.erb_spec.rb
+++ spec/views/games/index.html.erb_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "games/index", type: :view do
+RSpec.describe 'games/index', type: :view do
   before(:each) do
     assign(:games, [
-      Game.create!(
-        :name => "Name"
-      ),
-      Game.create!(
-        :name => "Name"
-      )
-    ])
+             Game.create!(
+               name: 'Name'
+             ),
+             Game.create!(
+               name: 'Name'
+             )
+           ])
   end
 
-  it "renders a list of games" do
+  it 'renders a list of games' do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select 'tr>td', text: 'Name'.to_s, count: 2
   end
 end
