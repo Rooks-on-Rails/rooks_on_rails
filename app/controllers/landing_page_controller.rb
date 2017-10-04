60dbd10f6e6fc2dc7fbc6415d6827fae68d9a067
dpoit <dannypoit@gmail.com>
2017-10-04 14:32:07 -0400

    Fixed rubocop offenses

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,6 +1,3 @@
 class LandingPageController < ApplicationController
-
-    def index
-    end
-    
+  def index; end
 end
--- spec/views/games/show.html.erb_spec.rb
+++ spec/views/games/show.html.erb_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "games/show", type: :view do
+RSpec.describe 'games/show', type: :view do
   before(:each) do
     @game = assign(:game, Game.create!(
-      :name => "Name"
+                            name: 'Name'
     ))
   end
 
-  it "renders attributes in <p>" do
+  it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Name/)
   end
