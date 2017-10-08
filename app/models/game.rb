e5594658b5da2021642d1c0b07c3b42d54fc0e0e
Matt <mattwalker216@gmail.com>
2017-10-08 16:40:19 +0000

    Testing Git - edited Model game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,5 @@ class Game < ApplicationRecord
   has_one :white_player, class_name: 'User', dependent: false
   has_one :black_player, class_name: 'User', dependent: false
   has_one :winning_player, class_name: 'User', dependent: false
+  has_many :pieces
 end
