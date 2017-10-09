73a8f6917d09a953df9fe7ccac78e96e1f67861d
MuddyBootsCode <firsttogo98@gmail.com>
2017-10-09 11:19:29 +0000

    Corrected scope for Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  scope :available, -> { where(available: true) }
+  scope :available, -> { where (white_player_id(IS(NULL)) || black_player_id(IS(NULL))) }
   has_one :white_player, class_name: 'User', dependent: false
   has_one :black_player, class_name: 'User', dependent: false
   has_one :winning_player, class_name: 'User', dependent: false
