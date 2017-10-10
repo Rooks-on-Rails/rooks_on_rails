66c94d982785d988506cd0ae9e2903690cb1ea42
ddunbar88 <derekdunbar@mac.com>
2017-10-10 04:51:19 +0000

    Added very high overview to obstructed method within piece.rb

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,23 @@
 class Piece < ApplicationRecord
   belongs_to :game
   belongs_to :user
+
+  def obstructed?(destination_x, destination_y)
+    #Horizontal Obstructions
+    if condition
+  
+    #Vertical Obstructions
+    elsif condition
+      
+  
+    #Diagonal Obstructions
+    elsif condition
+      
+  
+    #Invalid Input  
+    else condition
+  
+  end
+
+  
 end
