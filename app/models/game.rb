088637e2b6ac9b23289e1fc10d8517cb39950efc
Matt <mattwalker216@gmail.com>
2017-10-13 02:40:03 +0000

    Updated Model game.rb to be current/fixed Travis Errors

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,37 +5,31 @@ class Game < ApplicationRecord
   has_one :winning_player, class_name: 'User', dependent: false
   has_many :pieces, dependent: false
 
-  attr_accessor :board_positions
-
-  def initialize
-    @board_positions = Array.new(8) { Array.new(8, ' ') }
-  end
-
   def populate_board!
-    @board_positions[0][0] = Rook.new
-    @board_positions[0][1] = Knight.new
-    @board_positions[0][2] = Bishop.new
-    @board_positions[0][3] = Queen.new
-    @board_positions[0][4] = King.new
-    @board_positions[0][5] = Bishop.new
-    @board_positions[0][6] = Knight.new
-    @board_positions[0][7] = Rook.new
+    Rook.create(position_x: 0, position_y: 0, game: self, color: 'black')
+    Knight.create(position_x: 0, position_y: 1, game: self, color: 'black')
+    Bishop.create(position_x: 0, position_y: 2, game: self, color: 'black')
+    Queen.create(position_x: 0, position_y: 3, game: self, color: 'black')
+    King.create(position_x: 0, position_y: 4, game: self, color: 'black')
+    Bishop.create(position_x: 0, position_y: 5, game: self, color: 'black')
+    Knight.create(position_x: 0, position_y: 6, game: self, color: 'black')
+    Rook.create(position_x: 0, position_y: 7, game: self, color: 'black')
 
-    @board_positions[7][0] = Rook.new
-    @board_positions[7][1] = Knight.new
-    @board_positions[7][2] = Bishop.new
-    @board_positions[7][3] = Queen.new
-    @board_positions[7][4] = King.new
-    @board_positions[7][5] = Bishop.new
-    @board_positions[7][6] = Knight.new
-    @board_positions[7][7] = Rook.new
+    Rook.create(position_x: 7, position_y: 0, game: self, color: 'white')
+    Knight.create(position_x: 7, position_y: 1, game: self, color: 'white')
+    Bishop.create(position_x: 7, position_y: 2, game: self, color: 'white')
+    Queen.create(position_x: 7, position_y: 3, game: self, color: 'white')
+    King.create(position_x: 7, position_y: 4, game: self, color: 'white')
+    Bishop.create(position_x: 7, position_y: 5, game: self, color: 'white')
+    Knight.create(position_x: 7, position_y: 6, game: self, color: 'white')
+    Rook.create(position_x: 7, position_y: 7, game: self, color: 'white')
 
     0.upto(7) do |b| # b to stand in for black for now
-      board[6][b] = Pawn.new
+      Pawn.create(position_x: 1, position_y: b, game: self, color: 'black')
     end
 
     0.upto(7) do |w| # w to stand in for white for now
-      board[1][w] = Pawn.new
+      Pawn.create(position_x: 6, position_y: w, game: self, color: 'white')
     end
   end
 end
