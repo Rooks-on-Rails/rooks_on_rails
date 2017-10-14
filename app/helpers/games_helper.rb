4374a16f58828d1e7919f78c47d319bf42d7c284
Matt <mattwalker216@gmail.com>
2017-10-14 19:59:27 +0000

    Fixing Travis Offenses

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,9 +1,8 @@
 module GamesHelper
-  
   def render_piece(x, y)
-    piece = @game.pieces.find_by(position_x: x, position_y: y)
+    @game.pieces.find_by(position_x: x, position_y: y)
     # return nil unless piece
 
-    image_tag("black_rook.svg", size: "48x48", alt: "Black Rook")
+    image_tag('black_rook.svg', size: '48x48', alt: 'Black Rook')
   end
 end
