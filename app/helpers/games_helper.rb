9b2cd77588de214a11df18cf1fe1713ebce4e999
Matt <mattwalker216@gmail.com>
2017-10-14 17:50:03 +0000

    Added method for render_piece and defined first square in show.html.erb

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,12 @@
 module GamesHelper
+
+  # def test
+  #   "This is a test"    
+  # end
+  def render_piece(x, y)
+    piece = @game.pieces.find_by(position_x, position_y)
+    return nil unless piece
+
+    image_tag("black_rook.svg")
+  end
 end
