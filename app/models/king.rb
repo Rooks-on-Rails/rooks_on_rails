2b64172af10fd31ca4bf7dcd3516e50549a4efc2
dpoit <dannypoit@gmail.com>
2017-10-15 15:57:55 -0400

    Created valid_move? method in king model

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,2 +1,11 @@
 class King < Piece
+  def valid_move?(destination_x, destination_y)
+    difference_x = destination_x - self.position_x
+    difference_y = destination_y - self.position_y
+
+    if difference_x.abs <= 1 && difference_y.abs <= 1
+      return true
+    end
+    false
+  end
 end
---
+++ spec/models/king_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe King, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe #model do
+    # something
+  end
+end
