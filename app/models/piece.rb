9fa8536d69337e3495204425b1ad0fdb269d5f68
MuddyBootsCode <firsttogo98@gmail.com>
2017-10-15 20:55:50 +0000

    added capture logic to piece model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,7 +2,21 @@ class Piece < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
+
   def icon_path
     "#{color}_#{type}.svg".downcase
+  end
+
+  def move_to!(x, y)
+    piece = Piece.find_by(position_x: x, position_y: y)
+
+    if piece.present? && piece.color != color
+      piece.update_attributes(position_x: nil, position_y: nil)
+    elsif piece.present? == false
+      update_attributes(position_x: x, position_y: y)
+    else
+      return 'invalid move'
+    end
+
   end
 end
