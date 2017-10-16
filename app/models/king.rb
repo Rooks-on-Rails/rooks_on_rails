ab4ecdd42fd1e274aba4163fab1d9d24280d36ab
dpoit <dannypoit@gmail.com>
2017-10-16 01:36:26 -0400

    Wrote tests for king model

--- app/models/king.rb
+++ app/models/king.rb
@@ -3,7 +3,7 @@ class King < Piece
     difference_x = destination_x - self.position_x
     difference_y = destination_y - self.position_y
 
-    if difference_x.abs <= 1 && difference_y.abs <= 1
+    if difference_x.abs == 1 || difference_y.abs == 1
       return true
     end
     false
--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -1,8 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe King, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-  describe #model do
-    # something
+  describe '#valid_move' do
+    it 'returns true if the move is one space diagonal' do
+      king = King.new
+      king.position_x = 3
+      king.position_y = 3
+      expect(king.valid_move?(4, 4)).to eq(true)
+    end
+
+    it 'returns true if the move is one space right' do
+      king = King.new
+      king.position_x = 3
+      king.position_y = 3
+      expect(king.valid_move?(4, 3)).to eq(true)
+    end
+
+    it 'returns true if the move is one space up' do
+      king = King.new
+      king.position_x = 3
+      king.position_y = 3
+      expect(king.valid_move?(3, 4)).to eq(true)
+    end
+
+    it 'returns true if the move is one space left' do
+      king = King.new
+      king.position_x = 3
+      king.position_y = 3
+      expect(king.valid_move?(3, 2)).to eq(true)
+    end
+
+    it 'returns true if the move is one space down' do
+      king = King.new
+      king.position_x = 3
+      king.position_y = 3
+      expect(king.valid_move?(2, 3)).to eq(true)
+    end
+
+    it 'returns false if the move is the same space' do
+      king = King.new
+      king.position_x = 3
+      king.position_y = 3
+      expect(king.valid_move?(3, 3)).to eq(false)
+    end
+
+    it 'returns false if the move is more than one space' do
+      king = King.new
+      king.position_x = 3
+      king.position_y = 3
+      expect(king.valid_move?(5, 3)).to eq(false)
+    end
   end
 end
