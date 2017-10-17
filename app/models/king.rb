79824f513dc25c34bf646d7b7b11d4505680c263
dpoit <dannypoit@gmail.com>
2017-10-16 20:26:09 -0400

    Fixed rubocop offenses

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,11 +1,9 @@
 class King < Piece
   def valid_move?(destination_x, destination_y)
-    difference_x = destination_x - self.position_x
-    difference_y = destination_y - self.position_y
+    difference_x = destination_x - position_x
+    difference_y = destination_y - position_y
 
-    if difference_x.abs == 1 || difference_y.abs == 1
-      return true
-    end
+    return true if difference_x.abs == 1 || difference_y.abs == 1
     false
   end
 end
