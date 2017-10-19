76812ab1013f9bc943e6c66fa9b3b37913b18f29
dpoit <dannypoit@gmail.com>
2017-10-18 22:40:51 -0400

    Refactored king model

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,11 +1,10 @@
 class King < Piece
   def valid_move?(destination_x, destination_y)
-    if destination_x.between?(0, 7) && destination_y.between?(0, 7)
-      difference_x = destination_x - position_x
-      difference_y = destination_y - position_y
-
-      return true if difference_x.abs == 1 || difference_y.abs == 1
-    end
-    false
+    return false unless destination_x.between?(0, 7) && destination_y.between?(0, 7)
+    difference_x = destination_x - position_x
+    difference_y = destination_y - position_y
+    return false if difference_x.zero? && difference_y.zero?
+    valid_move_distances = [0, 1]
+    valid_move_distances.include?(difference_x.abs) && valid_move_distances.include?(difference_y.abs)
   end
 end
