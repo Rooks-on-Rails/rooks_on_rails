6fe3012373139e6c58af20bd01ec862768f9c75e
dpoit <dannypoit@gmail.com>
2017-10-21 12:51:37 -0400

    Refactored and fixed rubocop offenses

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -3,12 +3,7 @@ class Knight < Piece
     return false unless destination_x.between?(0, 7) && destination_y.between?(0, 7)
     difference_x = destination_x - position_x
     difference_y = destination_y - position_y
-    if difference_x.abs == 1 && difference_y.abs == 2
-      return true
-    elsif difference_x.abs == 2 && difference_y.abs == 1
-      return true
-    else
-      return false
-    end
+    return true if difference_x.abs == 1 && difference_y.abs == 2 || difference_x.abs == 2 && difference_y.abs == 1
+    false
   end
 end
