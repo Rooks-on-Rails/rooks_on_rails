c7c44812ec9377b83fc88fe1e581b5cca491ba91
dpoit <dannypoit@gmail.com>
2017-10-21 12:41:31 -0400

    Method and tests for valid_move? for knight

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,14 @@
 class Knight < Piece
+  def valid_move?(destination_x, destination_y)
+    return false unless destination_x.between?(0, 7) && destination_y.between?(0, 7)
+    difference_x = destination_x - position_x
+    difference_y = destination_y - position_y
+    if difference_x.abs == 1 && difference_y.abs == 2
+      return true
+    elsif difference_x.abs == 2 && difference_y.abs == 1
+      return true
+    else
+      return false
+    end
+  end
 end
