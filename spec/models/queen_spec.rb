38b09a799190fed2a053fdc4d23d8e6a20835842
Matt <mattwalker216@gmail.com>
2017-10-22 19:00:42 +0000

    RSpec Tests for Diagonal Moves

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Queen, type: :model do
   describe '#valid_move' do
-    it 'returns true if the move is one space up' do
+    it 'returns true if the move is one space down' do
       queen = Queen.new
       queen.position_x = 0
       queen.position_y = 3
@@ -11,7 +11,7 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns true if the move is four spaces up' do
+    it 'returns true if the move is four spaces down' do
       queen = Queen.new
       queen.position_x = 0
       queen.position_y = 3
@@ -20,7 +20,7 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns false if the move is eight spaces up (off board)' do
+    it 'returns false if the move is eight spaces down (off board)' do
       queen = Queen.new
       queen.position_x = 0
       queen.position_y = 3
@@ -92,7 +92,7 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns true if the move is one space down' do
+    it 'returns true if the move is one space up' do
       queen = Queen.new
       queen.position_x = 7
       queen.position_y = 3
@@ -101,7 +101,7 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns true if the move is three spaces down' do
+    it 'returns true if the move is three spaces up' do
       queen = Queen.new
       queen.position_x = 7
       queen.position_y = 3
@@ -110,7 +110,7 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns false if the move is eight spaces down (off board)' do
+    it 'returns false if the move is eight spaces up (off board)' do
       queen = Queen.new
       queen.position_x = 7
       queen.position_y = 3
@@ -119,7 +119,7 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns false if the move is one space down (off board)' do
+    it 'returns false if the move is one space up (off board)' do
       queen = Queen.new
       queen.position_x = 0
       queen.position_y = 3
@@ -128,7 +128,7 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns true if the move is one space up from middle of board' do
+    it 'returns true if the move is one space down from middle of board' do
       queen = Queen.new
       queen.position_x = 4
       queen.position_y = 4
@@ -137,7 +137,7 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns true if the move is three spaces up from middle of board' do
+    it 'returns true if the move is three spaces down from middle of board' do
       queen = Queen.new
       queen.position_x = 4
       queen.position_y = 4
@@ -146,7 +146,7 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns false if the move is five spaces up from middle of board (off board)' do
+    it 'returns false if the move is five spaces down from middle of board (off board)' do
       queen = Queen.new
       queen.position_x = 4
       queen.position_y = 4
@@ -209,7 +209,7 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns true if the move is one space down from middle of board' do
+    it 'returns true if the move is one space up from middle of board' do
       queen = Queen.new
       queen.position_x = 4
       queen.position_y = 4
@@ -218,7 +218,7 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns true if the move is three spaces down from middle of board' do
+    it 'returns true if the move is three spaces up from middle of board' do
       queen = Queen.new
       queen.position_x = 4
       queen.position_y = 4
@@ -227,11 +227,119 @@ RSpec.describe Queen, type: :model do
   end
 
   describe '#valid_move' do
-    it 'returns false if the move is five spaces down from middle of board (off board)' do
+    it 'returns false if the move is five spaces up from middle of board (off board)' do
       queen = Queen.new
       queen.position_x = 4
       queen.position_y = 4
       expect(queen.valid_move?(-1, 4)).to eq(false)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns true if the move is one space diagonally up-right from middle of board' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(3, 5)).to eq(true)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns true if the move is three spaces diagonally up-right from middle of board' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(1, 7)).to eq(true)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns false if the move is five spaces diagonally up-right from middle of board (off board)' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(-1, 9)).to eq(false)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns true if the move is one space diagonally up-left from middle of board' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(3, 3)).to eq(true)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns true if the move is three spaces diagonally up-left from middle of board' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(1, 1)).to eq(true)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns false if the move is five spaces diagonally up-left from middle of board (off board)' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(-1, -1)).to eq(false)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns true if the move is one space diagonally down-left from middle of board' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(5, 3)).to eq(true)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns true if the move is three spaces diagonally down-left from middle of board' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(7, 1)).to eq(true)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns false if the move is five spaces diagonally down-left from middle of board (off board)' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(9, -1)).to eq(false)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns true if the move is one space diagonally down-right from middle of board' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(5, 5)).to eq(true)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns true if the move is three spaces diagonally down-right from middle of board' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(7, 7)).to eq(true)
+    end
+  end
+
+  describe '#valid_move' do
+    it 'returns false if the move is five spaces diagonally down-right from middle of board (off board)' do
+      queen = Queen.new
+      queen.position_x = 4
+      queen.position_y = 4
+      expect(queen.valid_move?(9, 9)).to eq(false)
     end
   end
 end
