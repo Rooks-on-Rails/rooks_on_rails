fcea012387272625e94b571caa6e2f3048cf97f6
ddunbar88 <derekdunbar@mac.com>
2017-10-22 16:02:15 +0000

    Changes to valid_move? on pawn.rb

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,2 +1,21 @@
 class Pawn < Piece
+
+ def first_move
+ end
+
+ def valid_move?(destination_x, destination_y)
+    return false if backwards_move?(destination_y)
+    return true if capture_move?(destination_x, destination_y)
+    return false if horizontal_move?(destination_x)     
+ end
+
+ def horizontal_move?(h)
+    difference_x = (position_x - h).abs
+    difference_x != 0
+ end
+
+ def backwards_move?(b)
+    color ? position_y > b : position_y < destination_y
+ end
+
 end
