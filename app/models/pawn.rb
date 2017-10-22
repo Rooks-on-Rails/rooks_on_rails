7125a57e11c1a68a78d29faa540c11084df9292c
ddunbar88 <derekdunbar@mac.com>
2017-10-22 19:21:12 +0000

    Added additional methods

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -3,12 +3,6 @@ class Pawn < Piece
  def first_move
  end
 
- def valid_move?(destination_x, destination_y)
-    return false if backwards_move?(destination_y)
-    return true if capture_move?(destination_x, destination_y)
-    return false if horizontal_move?(destination_x)     
- end
-
  def horizontal_move?(h)
     difference_x = (position_x - h).abs
     difference_x != 0
@@ -16,6 +10,18 @@ class Pawn < Piece
 
  def backwards_move?(b)
     color ? position_y > b : position_y < destination_y
+ end
+
+ def valid_move?(destination_x, destination_y)
+    return false if backwards_move?(destination_y)
+    return true if capture_move?(destination_x, destination_y)
+    return false if horizontal_move?(destination_x)     
+ end
+
+ def vertical_capture
+ end
+
+ def horizontal_capture
  end
 
 end
