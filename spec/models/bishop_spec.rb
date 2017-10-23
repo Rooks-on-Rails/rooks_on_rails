0bef11e3149c3ebc95acd8f6696d6814acf878af
MuddyBootsCode <firsttogo98@gmail.com>
2017-10-23 19:16:47 +0000

    making mike edits

--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 RSpec.describe Bishop, type: :model do
   describe '#valid_move?' do
     let(:game) { Game.create! }
-    let(:user) { User.create!(email: 'dude@abides.com', password: 'thatsyouropinionman') }
     let(:bishop) { Bishop.create!(position_x: 2, position_y: 0, game: game, color: 'white') }
 
     it 'returns true if moves are diagonal' do
@@ -13,13 +12,11 @@ RSpec.describe Bishop, type: :model do
     end
 
     it "shouldn't allow you to move in a straight direction" do
-      bishop.reload
       expect(bishop.valid_move?(1, 0)).to eq(false)
       expect(bishop.valid_move?(2, 1)).to eq(false)
     end
 
     it "shouldn't allow you to move off the board" do
-      bishop.reload
       expect(bishop.valid_move?(6, 8)). to eq(false)
       expect(bishop.valid_move?(-1, 3)). to eq(false)
     end
