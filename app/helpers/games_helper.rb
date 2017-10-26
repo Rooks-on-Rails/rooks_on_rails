cb13bc1cc8697a6e15e69d03c5680f8916bcd269
Matt <mattwalker216@gmail.com>
2017-10-26 01:56:04 +0000

    Getting closer

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,7 +2,8 @@ module GamesHelper
   def render_piece(x, y)
     piece = @game.pieces.find_by(position_x: x, position_y: y)
     return nil if piece.blank?
-
-    image_tag(piece.icon_path, size: '48x48')
+    
+    link_to image_tag(piece.icon_path, size: '48x48'), piece_path(piece[:id])
+    if piece == @current_piece 
   end
 end
