818abd7ee93bb49f846fe38ae57e0a7d24ba23d4
Matt <mattwalker216@gmail.com>
2017-10-26 03:37:42 +0000

    Removed the game_spec code Danny and I were working on

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -179,18 +179,4 @@ RSpec.describe Game, type: :model do
       expect(game.pieces.find_by(position_x: 0, position_y: 3).type).to eq 'Queen'
     end
   end
-
-  describe '#check?' do
-    it 'returns true if the king is in check' do
-      king = King.new(position_x: 4, position_y: 4, color: 'black')
-      knight = Knight.new(position_x: 2, position_y: 3, color: 'white')
-      expect(knight.valid_move?(king.position_x, king.position_y) && knight.color != king.color).to eq(true)
-    end
-
-    it 'returns false if the king is not in check' do
-      king = King.new(position_x: 4, position_y: 4, color: 'black')
-      knight = Knight.new(position_x: 2, position_y: 3, color: 'black')
-      expect(knight.valid_move?(king.position_x, king.position_y) && knight.color != king.color).to eq(true)
-    end
-  end
 end
