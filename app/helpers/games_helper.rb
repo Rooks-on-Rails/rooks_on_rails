46a7d17e331f5192c96512ce4368e53287935bd6
Matt <mattwalker216@gmail.com>
2017-10-29 13:31:02 +0000

    Adding '.html_safe' and/or 'raw' but still struggling

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -4,6 +4,11 @@ module GamesHelper
     return nil if piece.blank?
     
     link_to image_tag(piece.icon_path, size: '48x48'), piece_path(piece[:id])
-    if piece == @current_piece 
+    if piece == @current_piece
+      "<div class='selected'>".html_safe
+    else 
+      "<div>".html_safe
+    end
+    "</div>".html_safe
   end
 end
