c4f91952260c4926d9b424334183d52bb4a3a8f9
Matt <mattwalker216@gmail.com>
2017-10-31 01:44:09 +0000

    Fixed Rubocop Errors

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,11 +2,11 @@ module GamesHelper
   def render_piece(x, y)
     piece = @game.pieces.find_by(position_x: x, position_y: y)
     return nil if piece.blank?
-    
+
     # link_to image_tag(piece.icon_path, size: '48x48'), piece_path(piece[:id])
     if piece == @current_piece
-      content_tag(:div, image_tag(piece.icon_path, size: '48x48'), class: "selected")
-    else 
+      content_tag(:div, image_tag(piece.icon_path, size: '48x48'), class: 'selected')
+    else
       link_to image_tag(piece.icon_path, size: '48x48'), piece_path(piece[:id])
     end
   end
