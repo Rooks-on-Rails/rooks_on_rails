695230df84e0259916eadd9445fdeca2579939d0
dpoit <dannypoit@gmail.com>
2017-11-05 22:43:35 -0500

    Actually, the last commit was wrong. This one fixes it.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,6 +2,7 @@ module GamesHelper
   def render_piece(x, y)
     piece = @game.pieces.find_by(position_x: x, position_y: y)
     return nil if piece.blank?
+    
     image_tag(piece.icon_path, size: '48x48')
   end
 end
