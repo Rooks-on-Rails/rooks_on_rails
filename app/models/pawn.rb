f245f7fea819be21672ffa46e9b545238743fa4a
dpoit <dannypoit@gmail.com>
2017-11-08 02:18:50 -0500

    Updated valid move method

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,19 +1,29 @@
 class Pawn < Piece
-  initial_white_pawn_locations = [[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1]]
-  initial_black_pawn_locations = [[0, 6], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6], [6, 6], [7, 6]]
-
-  def initial_pawn_location?
-    if self.color == 'white'
-      initial_white_pawn_locations.include?([self.position_x, self.position_y])
-    else
-      initial_black_pawn_locations.include?([self.position_x, self.position_y])
-    end
-  end
-
   def valid_move?(destination_x, destination_y)
     return false unless destination_x.between?(0, 7) && destination_y.between?(0, 7)
-    return false if is_obstructed?(destination_x, destination_y)
-    difference_x = destination_x - position_x
-    difference_y = destination_y - position_y
+    # return false if is_obstructed?(destination_x, destination_y)
+    # include the above line once that method is added to the piece model
+
+    possible_moves = []
+
+    if color == 'white'
+      possible_moves << [position_x.to_i, position_y.to_i + 1]
+      if position_y = 1
+        possible_moves << [position_x.to_i, position_y.to_i + 2]
+      end
+    end
+
+    if color == 'black'
+      possible_moves << [position_x.to_i, (position_y.to_i - 1)]
+      if position_y = 6
+        possible_moves << [position_x.to_i, (position_y.to_i - 2)]
+      end
+    end
+
+    if possible_moves.include?([destination_x, destination_y])
+      return true
+    end
+
+    false
   end
 end
