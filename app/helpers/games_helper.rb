d47ccdd7037e24c89de40c34e368798760925f23
Matt <mattwalker216@gmail.com>
2017-11-08 02:35:08 +0000

    Issue with nil

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,20 +1,18 @@
 module GamesHelper
   def render_piece(x, y)
     piece = @game.pieces.find_by(position_x: x, position_y: y)
-    # return nil if piece.blank?
 
-    # link_to image_tag(piece.icon_path, size: '48x48'), piece_path(piece[:id])
     if piece == @current_piece
       content_tag(:div, image_tag(piece.icon_path, size: '48x48'), class: 'selected')
     else
       if piece.present? 
         link_to image_tag(piece.icon_path, size: '48x48'), piece_path(piece[:id])
       else
-        form_for(@current_piece, url: piece_path(@current_piece)) do |f|
-          f.hidden_field(:position_x, value: x) +
-          f.hidden_field(:position_y, value: y) +
-          f.submit 'Save'
-        end #install simple_form
+        simple_form_for(@current_piece, url: piece_path(@current_piece)) do |f|
+          # f.hidden_field(:position_x, value: x) +
+          # f.hidden_field(:position_y, value: y) +
+          f.submit 'Update'
+        end
       end
     end
   end
