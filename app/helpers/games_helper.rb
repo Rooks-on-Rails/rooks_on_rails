f67c23b0676896ec5f4af19667531ba07d35cb08
Matt <mattwalker216@gmail.com>
2017-11-12 16:46:29 +0000

    pieces#show page works, can't update piece position though

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,19 +2,18 @@ module GamesHelper
   def render_piece(x, y)
     piece = @game.pieces.find_by(position_x: x, position_y: y)
 
-    if piece == @current_piece
-      content_tag(:div, image_tag(piece.icon_path, size: '48x48'), class: 'selected')
-    elsif piece.present? 
-      link_to image_tag(piece.icon_path, size: '48x48'), piece_path(piece[:id])
-    else
-      # "Testing"
-      simple_form_for(@current_piece, url: piece_path(@current_piece)) do |f|
-        # f.input position_x, as: :hidden, input_html: {value: x} 
-        # f.input position_y,
-        # f.hidden_field(:position_x, value: x) +
-        # f.hidden_field(:position_y, value: y) +
-        f.submit 'Update'
-      end
+    if piece.present? 
+      link_to image_tag(piece.icon_path, size: '48x48'), piece_path(piece[:id])      
     end
   end
 end
+# piece == nil
+#       simple_form_for(@current_piece, url: piece_path(@current_piece)) do |f|
+#         f.input position_x, as: :hidden, input_html: {value: x} 
+#         f.input position_y, as: :hidden, input_html: {value: y}
+#         f.submit 'Update'
+#       end
+
+
+# piece == @current_piece
+#       content_tag(:div, image_tag(piece.icon_path, size: '48x48'), class: 'selected')
--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -1,2 +1,18 @@
 module PiecesHelper
+  def render_piece_show(x, y)
+    piece = @game.pieces.find_by(position_x: x, position_y: y)
+
+    if piece == nil
+      simple_form_for(@current_piece, url: piece_path(@current_piece)) do |f|
+        f.input :position_x, as: :hidden, input_html: {value: x} 
+        f.input :position_y, as: :hidden, input_html: {value: y}
+        f.submit 'Update'
+      end
+    elsif piece == @current_piece
+      content_tag(:div, image_tag(piece.icon_path, size: '48x48'), class: 'selected')
+      binding.pry
+    else
+      link_to image_tag(piece.icon_path, size: '48x48'), piece_path(piece[:id])      
+    end
+  end
 end
