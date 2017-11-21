7654ccae6b0203668b51ce42e0b2f552eeba6f6d
Matt <mattwalker216@gmail.com>
2017-11-21 01:35:04 +0000

    Fixed Rubocop Errors

--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -4,9 +4,9 @@ module PiecesHelper
 
     if piece.nil?
       simple_form_for(@current_piece, url: piece_path(@current_piece), as: :piece) do |f|
-        f.input(:position_x, as: :hidden, input_html: {value: x}) +
-        f.input(:position_y, as: :hidden, input_html: {value: y}) +
-        f.submit('Update')
+        f.input(:position_x, as: :hidden, input_html: { value: x }) +
+          f.input(:position_y, as: :hidden, input_html: { value: y }) +
+          f.submit('Update')
       end
     elsif piece == @current_piece
       content_tag(:div, image_tag(piece.icon_path, size: '48x48'), class: 'selected')
